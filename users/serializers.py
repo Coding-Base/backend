689c07b8f05a2251ed34ff50cea58f7c650d2b65
Coding-Base <osimigodsgiftgbubemi@gmail.com>
2025-02-13 23:56:49 +0100

    updated my serializer

--- users/serializers.py
+++ users/serializers.py
@@ -2,28 +2,30 @@ from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username',  'email', ]
-
+        fields = ['id', 'username', 'email']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+
     class Meta:
         model = User
-        fields = [ 'username', 'email', 'password',  ]
+        fields = ['username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_username(self, value):
+        # Allow any username, including those with spaces.
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            # full_name=validated_data['full_name'],
             password=validated_data['password']
         )
         return user
-
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
