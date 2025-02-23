51b42c2d86eb118ee02fd91aa8bb6dfc726ea35f
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-02-24 00:30:41 +0100

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# Unregister the default User admin and re-register with custom fields to display details
+admin.site.unregister(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+
+admin.site.register(User, UserAdmin)
