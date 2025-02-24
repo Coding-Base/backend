c8546e7c03daaabb87cbe08af7ac858444ec60a7
Osimi Godsgift <114400012+Coding-Base@users.noreply.github.com>
2025-02-24 10:45:04 +0100

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+User = get_user_model()
 
 try:
     admin.site.unregister(User)
@@ -11,4 +13,3 @@ class CustomUserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
 
 admin.site.register(User, CustomUserAdmin)
-
